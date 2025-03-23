b094c66056514786d85743285b6a490ad63f1e22
Luxiang Lin <linluxiang@gmail.com>
2025-03-23 23:58:49 +0100

    34.

---
+++ 34. Find First and Last Position of Element in Sorted Array/ans.go
@@ -0,0 +1,48 @@
+func binarySearchFirst(nums []int, target int) int {
+    i, j := -1, len(nums)
+    for i != j - 1 {
+        mid := i + (j-i) >> 1
+
+        if nums[mid] < target {
+            i = mid
+        } else if nums[mid] >= target {
+            j = mid
+        }
+    }
+    return j
+}
+
+func binarySearchLast(nums []int, target int) int {
+    i, j := -1, len(nums)
+    for i + 1 != j {
+        mid := i + (j-i) >> 1
+
+        if nums[mid] <= target {
+            i = mid
+        } else if nums[mid] > target {
+            j = mid
+        }
+    }
+    return i
+}
+
+func searchRange(nums []int, target int) []int {
+    if len(nums) == 0 {
+        return []int{-1, -1}
+    }
+
+    first := binarySearchFirst(nums, target)
+    fmt.Println("first: ", first)
+    if first >= len(nums) || nums[first] != target {
+        first = -1
+    }
+
+    last := binarySearchLast(nums, target)
+    fmt.Println("last: ", last)
+    if last < 0 || nums[last] != target {
+        last = -1
+    }
+
+    return []int{first, last}
+
+}
