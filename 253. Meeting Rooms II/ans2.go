37288fa9a77537b7eccc5a29eacd82328e2c5efc
Luxiang Lin <linluxiang@gmail.com>
2025-04-01 15:14:16 +0200

    253

--- 253. Meeting Rooms II/ans2.go
+++ 253. Meeting Rooms II/ans2.go
@@ -8,10 +8,10 @@ func minMeetingRooms(intervals [][]int) int {
         return a[1] - b[1]
     })
 
-    meetingRooms := [][]int{}
+    meetingRooms := [][]int{} // 这里，会议室可以用一个最小堆来组织，堆的顶端是最早结束的会议，所以堆的比较用的是interval[1]
 loop:
     for _, entry := range intervals {
-        for i, m := range meetingRooms {
+        for i, m := range meetingRooms { // 在这里只需要查看堆顶的元素是否有空，有空的话就弹出。
             if m[1] <= entry[0] {
                 // can use the same room
                 meetingRooms[i] = entry
